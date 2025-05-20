f29816872fe7449fa85a10963f3fb00ca51da1f4
Jhonny Miranda <jhonnyrs22@hotmail.com>
2025-05-20 16:18:49 -0600

    image

--- contenido.php
+++ contenido.php
@@ -11,7 +11,7 @@
             <a href="https://www.php.net/" class="btn btn-primary btn-lg">Explora más</a>
         </div>
         <div class="col-md-5 text-center">
-            <img src="../img/php.jpg" class="img-fluid rounded shadow" alt="Tecnología y medios">
+            <img src="https://rockcontent.com/es/wp-content/uploads/sites/3/2020/08/qu%C3%A9-es-PHP-1024x576.png.webp" class="img-fluid rounded shadow" alt="Tecnología y medios">
         </div>
     </div>
     <hr class="my-5">
